bf805b713c9578fbe03fa81c884603db55d2e153
Artem Kolesnik <steeltouch27@gmail.com>
2023-08-20 22:21:07 +0700

    Change get_profile endpoint path

--- my-vpn-backend/get_profile.py
+++ my-vpn-backend/get_profile.py
@@ -4,6 +4,6 @@ from ovpn_profile_service import OvpnProfileService
 
 get_profile_router = APIRouter()
 
-@get_profile_router.get("/profile")
+@get_profile_router.get("/profile.ovpn")
 async def get_profile():
     return FileResponse(path=OvpnProfileService.create_profile(),  media_type="application/binary")
